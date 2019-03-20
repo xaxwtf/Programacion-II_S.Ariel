2aaf76caa8ed2fe5e3f77163aa5abeb83a8357ab
xaxwtf <36156872+xaxwtf@users.noreply.github.com>
2019-03-20 11:50:03 -0300

    Add files via upload
    Terminar

---
+++ Sariel_Programacion II/Programacion II/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sello.Color = ConsoleColor.Blue;
+            Sello.mensaje = "hola";
+            Console.WriteLine(Sello.Imprimir());
+            Sello.ImprimirenColor();
+            Console.WriteLine(Sello.Imprimir());
+            Console.ReadKey();
+            Sello.Borrar();
+
+
+        }
+    }
+}
---
+++ Sariel_Programacion II/Programacion II/ConsoleApp1/Sello.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public  static class Sello
+    {
+        public static string mensaje;
+        public static ConsoleColor Color;
+        public static string Imprimir()
+        {
+            return Sello.mensaje;
+        }
+        public static void Borrar()
+        {
+            Sello.mensaje = " ";
+        }
+        public static void ImprimirenColor()
+        {
+            Console.ForegroundColor = Sello.Color;
+            Console.WriteLine(Sello.Imprimir());
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+        private static string ArmarformatodeMensaje()
+        {
+            string Techo =" ";
+            int tam = Sello.mensaje.Length;
+            for(int i=-2;i<=tam;i++)
+            {
+                Techo += "*";
+            }
+            return Techo + "\n*" + Sello.mensaje + "*\n"+ Techo;
+        }
+
+
+    }
+}
